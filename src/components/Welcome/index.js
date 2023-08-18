7cda0ee407f24e6b76767b71f7ce6ae52b1088a6
Hema <hemavathi.gadi@gmail.com>
2023-08-18 09:53:42 +0000

    Creating My Blog

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,26 +3,181 @@ import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
-  state = {isSubscribed: false}
-
-  onSubscribe = () => {
-    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
-  }
-
-  getSubscription = () => {
-    const {isSubscribed} = this.state
-    return isSubscribed ? 'Subscribed' : 'Subscribe'
-  }
-
   render() {
-    const subscribeCondition = this.getSubscription()
     return (
-      <div className="container">
-        <h1 className="title">Welcome</h1>
-        <p className="description">Thank you! Happy Learning</p>
-        <button type="button" onClick={this.onSubscribe}>
-          {subscribeCondition}
-        </button>
+      <div className="app-container">
+        <div className="html-projects-container">
+          <h1>HTML, CSS AND JAVASCRIPT BASED PROJECTS</h1>
+          <ul className="container">
+            <li>
+              <a
+                href="https://hemaform.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LOGIN FORM
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://defuseabomb.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                DEFUSE BOMB
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemavathitodoap.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                TODO LIST
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemasgrocery.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GROCERY LIST
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemaguesinggame.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GUESSING GAME
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://counterbyhema.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                COUNTER
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://catvsiblybyhema.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CAT VISIBILITY
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemarestaurent.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                RESTAURENT
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemawikisearch.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WIKI SEARCH
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1>REACT PROJECTS</h1>
+          <ul>
+            <li>
+              <a
+                href="https://hemaspeedometer.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SPEEDO METER
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemafruitcount.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                FRUITS EATEN
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://maintainusers.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MAINTAINING USERS
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://creatrandno.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                PICK RANDOM NUMBER
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemadestnsnsrch.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SEARCH DESTINATION
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemalettercount.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                COUNT NUMBER OF LETTERS ENTERED
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemadebugtodo.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CREATE TODO LIST
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemadebggglsrch.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GOOGLE SEARCH MODEL
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://hemadebugcaswi.ccbp.tech"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CASH WITHDRAW
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
